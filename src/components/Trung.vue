a3ba9e1f46389905a793e85d8e76386292771dee
Trung Hong Ta <trung.ta@hdwebsoft.co>
2019-01-18 17:25:44 +0700

    .

---
+++ src/components/Trung.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Contact</h1>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class Trung extends Vue {
+  @Prop() private trung!: string;
+}
+</script>
+<style lang="scss">
+</style>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="contact">
     <h1>This is an contact page</h1>
+    <Trung trung="hello trung"/>
   </div>
 </template>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Trung from "@/components/Trung.vue"; // @ is an alias to /src
+@Component({
+  components: {
+    Trung
+  }
+})
+export default class Contact extends Vue {}
+</script>
